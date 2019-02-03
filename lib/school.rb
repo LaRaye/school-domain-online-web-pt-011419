1912d1eccc4a666c99087c0c01d883fe4b278bc1
LaRaye Johnson <lnj2105@gmail.com>
2019-02-03 19:50:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,7 @@ class School
   end
   
   def add_student(student_name, grade)
-    #@roster[grade] = [] 
+    @roster[grade] = [] 
     @roster[grade] ||= grade
     @roster[grade] << student_name
   
